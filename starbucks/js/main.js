2c0c53f49b4bf6070948cd35d00844445d5fe5a8
sabeom <sabumi2002@naver.com>
2021-09-10 16:23:42 +0900

    3d animation

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -93,14 +93,23 @@ promotionToggleBtn.addEventListener('click', function () {
   }
 });
 
-function floatingObject(selector) {
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+function floatingObject(selector, delay, size) {
   // gsap.to(요소, 시간, 옵션);
-  gsap.to(selector, 1, {
-    y: 20,
+  gsap.to(selector, random(1.5, 2.5), {
+    y: size,
     repeat: -1, // 무한 반복
-    yoyo: true,
-    ease: Power1.easeInOut,
-    delay: 1
+    yoyo: true, // 실행한거 반대로 다시 실행
+    ease: Power1.easeInOut, // gsap easing- 느려졌다 빨라졌다 느려졌다 자연스럽게 만들어줌
+    delay: random(0, delay)
   });
 }
-floatingObject('.floating');
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
